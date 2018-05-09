9da4bbf6d7bc6d586763e29ff9e43c185b9202de
Lida <lidiarosvega@gmail.com>
2018-05-09 14:45:00 +0200

    version segunda

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -13,11 +13,14 @@ class PizzaController extends Controller
     public function index()
     {
     	$vector = array("Margarita", "Carbonara", "Hawaiana");
-    	$vectordos = array("Tomate", "Mozzarela", "Oregano", "Bacon");
+    	$vectorlocal = array("SanVicente", "Alburquerque", "LaCodosera");
+
         return $this->render('pizza/index.html.twig', [
             'controller_name' => 'PizzaController',
             'minombre' => 'Lidia',
-            'ingredientes' => $vectordos
+            'pizzas' => $vector,
+            'localidades' => $vectorlocal
+            
         ]);
     }
 
@@ -28,8 +31,43 @@ class PizzaController extends Controller
     public function nuevaPizza()
     {
     	
-        return $this->render('pizza/index.html.twig', [
+
+        return $this->render('pizza/nuevo.html.twig', [
+           
+        ]);
+    }
+    /**
+     * @Route("/pizza/editar", name="pizza_editar")
+     */
+    public function editarPizza()
+    {
+    	$vectorlocal = array("SanVicente", "Alburquerque", "LaCodosera");
+
+        return $this->render('pizza/editar.html.twig', [
+            'localidades' => $vectorlocal
+        ]);
+    }
+    /**
+     * @Route("/pizza/mostrar", name="pizza_mostrar")
+     */
+    public function mostrarPizza()
+    {
+    	
+        return $this->render('pizza/mostrar.html.twig', [
             
+        ]);
+    }
+    
+     /**
+     * @Route("/pizza/calcular/{parametro}", name="pizza_calcular", requeriments={"precio"="\d+"})
+     */
+    public function calcularPizza($precio)
+    {
+        $final = $precio * 1.21;
+        return $this->render('pizza/nombre.html.twig', [
+            
+            'preciofinal' => $final
+
         ]);
     }
 }
