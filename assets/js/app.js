6511d61c3f53aae708743dd7a8964b06bcdc121c
Lida <lidiarosvega@gmail.com>
2018-05-18 10:50:31 +0200

    webpack

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+
+
+
+//assets/js/app.js
+
+
+$(document).ready(function(){
+	console.log("hola");
+	console.log("adios");
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
+// webpack.config.js
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+    // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
+
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
+
+    // will create public/build/app.js and public/build/app.css
+    .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('appstyle', './assets/css/app.css')
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    // enable source maps during development
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
 
-    // uncomment if you use Sass/SCSS files
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+
+    // create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning()
+
+    // allow sass/scss files to be processed
     // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
 ;
 
+// export the final configuration
 module.exports = Encore.getWebpackConfig();
